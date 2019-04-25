2e4b4ea905a9544a00303f0c773bc61b45a85b8e
ParaschivGeorge <georgegmtt@gmail.com>
2019-04-25 17:51:56 +0300

    added repo annotation

--- src/main/java/com/paraett/usersservice/repository/ProjectRepository.java
+++ src/main/java/com/paraett/usersservice/repository/ProjectRepository.java
@@ -4,8 +4,10 @@ import com.paraett.usersservice.model.entities.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+@Repository
 public interface ProjectRepository extends JpaRepository<Project, Long>, JpaSpecificationExecutor<Project> {
     @Transactional
     @Modifying
